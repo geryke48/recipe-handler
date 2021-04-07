fd1dba95fa62129265b4533b82d6ace39d517120
Egri Ferenc <nagyonmindegy4@gmail.com>
2021-04-07 10:30:33 +0200

    Code beautifying

--- backend/src/controllers/recipe-controller.ts
+++ backend/src/controllers/recipe-controller.ts
@@ -1,13 +1,28 @@
+import { wrap } from '@mikro-orm/core';
 import { Router } from 'express';
 import { Recipe } from '../entities/recipe';
 
 export const recipeRouter = Router();
 
 recipeRouter
+    .use((req, res, next) => {
+        req.recipeRepository = req.orm.em.getRepository(Recipe);
+        next();
+    })
     .get('', async (req, res) => {
-        const recipeRepository = req.orm.em.getRepository(Recipe);
-        const recipes = await recipeRepository.findAll();
+        const recipes = await req.recipeRepository!.findAll();
         res.send(recipes);
     })
-    .post('', () => {})
+    .post('', async (req, res) => {
+        const recipe = new Recipe();
+
+        const wrappedRecipe = wrap(recipe);
+
+        wrappedRecipe.assign(req.body);
+
+        await req.recipeRepository!.persistAndFlush(recipe);
+
+        res.send(recipe);
+
+    })
     .get('/:id', () => {})
--- backend/src/request-extension.d.ts
+++ backend/src/request-extension.d.ts
@@ -1,9 +1,11 @@
 import { EntityRepository, IDatabaseDriver, MikroORM } from "@mikro-orm/core";
+import { Recipe } from "./entities/recipe";
 
 declare global {
     namespace Express {
         interface Request {
             orm: MikroORM<IDatabaseDriver>;
+            recipeRepository?: EntityRepository<Recipe>;
         }
     }
 }
