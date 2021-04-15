90ef820cd41a7b56c0a866567eb9b198c13e5915
Egri Ferenc <nagyonmindegy4@gmail.com>
2021-04-15 10:19:41 +0200

    DEL / PUT methods

--- backend/src/controllers/recipe-controller.ts
+++ backend/src/controllers/recipe-controller.ts
@@ -37,3 +37,22 @@ recipeRouter
         const recipes = await req.recipeRepository!.find({ name: req.params.name });
         res.send(recipes);
     })
+
+    .delete('/:id', async (req, res) => {
+        await req.recipeRepository!.nativeDelete({ id: Number(req.params.id)});
+        res.sendStatus(200);
+    })
+
+    .put('/:id', async (req, res) => {
+        const recipe = await req.recipeRepository!.findOne({ id: Number(req.params.id) }, ['ingredients']);
+        if(!recipe){
+            res.sendStatus(404);
+        }
+        const wrappedRecipe = wrap(recipe);
+
+        wrappedRecipe.assign(req.body, { em: req.orm.em });
+
+        await req.recipeRepository!.persistAndFlush(recipe);
+
+        res.send(recipe);
+    })
