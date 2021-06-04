af1fd74df22e7ea73411c89c534afbb7d8c42e29
Egri Ferenc <nagyonmindegy4@gmail.com>
2021-06-04 14:18:17 +0200

    Added menu

--- frontend/recipeHandler-client/src/app/app-routing.module.ts
+++ frontend/recipeHandler-client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddIngredientComponent } from './add-ingredient/add-ingredient.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'addRecipe',
+  component: AddRecipeComponent,
+},{
+  path: 'addIngerdient',
+  component: AddIngredientComponent,
+},{
+  path: '**',
+  redirectTo: '/addRecipe',
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/recipeHandler-client/src/app/app.module.ts
+++ frontend/recipeHandler-client/src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule}  from '@angular/material/list';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { AddIngredientComponent } from './add-ingredient/add-ingredient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AddRecipeComponent,
+    AddIngredientComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
